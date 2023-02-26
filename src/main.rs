ab0472a34107f2c4ddc19df675955b9811a4c200
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-02-26 17:37:00 -0300

    Creating an actix server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, news!");
+use actix_web::{App, HttpServer};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new())
+        .bind(("127.0.0.1", 3000))?
+        .run()
+        .await
 }
