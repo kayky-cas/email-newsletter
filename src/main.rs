e2b824d0f50f86fd2938f31eb591430a92772d17
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-02-26 21:30:03 -0300

    Subscribe end point

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use std::net::TcpListener;
-
-use email_newsletter::run;
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let port = 3000;
 
-    let listener = TcpListener::bind(("127.0.0.1", port))
+    let listener = std::net::TcpListener::bind(("127.0.0.1", port))
         .unwrap_or_else(|_| panic!("The port {} is not avalible", port));
 
-    run(listener)?.await
+    email_newsletter::run(listener)?.await
 }
